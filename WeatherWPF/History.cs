f71673a9f30dd30e1538efb5f3278a06af1ca6de
NikolayKharchenko <kharchenko1991@outlook.com>
2016-10-23 22:07:03 +0300

    Add class History.
    It's begin, only constructor

---
+++ WeatherWPF/History.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+class History
+{
+    public List<City> history { get; set; } // список <городов> у файле 
+
+    public History()   // конструктор к переменной history
+    {
+        String AllCitiesData = File.ReadAllText("city.list.json");
+        var AllCities = JsonConvert.DeserializeObject<List<City>>(AllCitiesData);   // - это ВСЕ города
+
+        List<String> GetHistoryFromFile = File.ReadAllLines("history.txt").ToList();
+
+        List<int> KeeperFromFile = null;                                            // - это города записаные у файл
+        foreach (string x in GetHistoryFromFile)
+        {
+            int id = Convert.ToInt32(x);
+            KeeperFromFile.Add(id);
+        }
+        history = AllCities.Where(X => KeeperFromFile.Contains(X._id)).ToList();
+    }   
+}
