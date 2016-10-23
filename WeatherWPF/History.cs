445841ff41b2b0fb9089906fb453f85633b85952
NikolayKharchenko <kharchenko1991@outlook.com>
2016-10-23 23:53:44 +0300

    Add realize History by file.txt
    Sorry, its commit consist of too many files.

--- WeatherWPF/History.cs
+++ WeatherWPF/History.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using Newtonsoft.Json;
 using System.IO;
 
-class History
+public class History
 {
     public List<City> history { get; set; } // список <городов> у файле 
 
@@ -15,12 +15,27 @@ class History
 
         List<String> GetHistoryFromFile = File.ReadAllLines("history.txt").ToList();
 
-        List<int> KeeperFromFile = null;                                            // - это города записаные у файл
+        if (GetHistoryFromFile.Count > 10)                                          // - обрезка до 10 на приеме
+            GetHistoryFromFile.RemoveRange(0, GetHistoryFromFile.Count - 10);
+
+        List<int> KeeperFromFile = new List<int>();                                 // - это города записаные у файл
         foreach (string x in GetHistoryFromFile)
         {
             int id = Convert.ToInt32(x);
             KeeperFromFile.Add(id);
         }
+
         history = AllCities.Where(X => KeeperFromFile.Contains(X._id)).ToList();
-    }   
+    }
+
+    public List<City> AddCity(int ID)
+    {
+        if (history.Count > 10)
+        {
+            history.RemoveAt(0);
+            history.Add(history.First(s => s._id == ID));
+            return history;
+        }
+        return history;
+    }
 }
